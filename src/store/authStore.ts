4c4eb2f4b7a6a65ac01cf88b7540568c3f82d4a0
komalbainsla2007-spec <komalbainsla2007@gmail.com>
2026-04-18 11:07:14 +0530

    Add Zustand store for authentication management

---
+++ src/store/authStore.ts
@@ -0,0 +1,11 @@
+// Authentication Zustand store
+import create from 'zustand';
+
+const useAuthStore = create((set) => ({
+  user: null,
+  isAuthenticated: false,
+  login: (user) => set({ user, isAuthenticated: true }),
+  logout: () => set({ user: null, isAuthenticated: false }),
+}));
+
+export default useAuthStore;
